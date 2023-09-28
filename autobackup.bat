6c85036a9890efbaaa7d1c594f2176e8ed2adf30
Lukasz Sajna <lukasz.sajna@gmail.com>
2023-09-28 06:01:28 +0200

    Regular auto-commit at 06:01:28 on 28/09/2023

---
+++ autobackup.bat
@@ -0,0 +1,11 @@
+cd "C:\Program Files (x86)\Steam\userdata"
+
+git add .
+set time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+set time=%time: =0%
+
+set date=%DATE:~0,2%/%DATE:~3,2%/%DATE:~6,4%
+set date=%date: =0%
+
+git commit -am "Regular auto-commit at %time% on %date%"
+git push origin master
