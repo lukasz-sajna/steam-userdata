f91fa763d59353ba1c5d4db6bb21b537e09a710e
Lukasz Sajna <lukasz.sajna@gmail.com>
2025-01-31 23:06:45 +0100

    Regular auto-commit at 23:06:45 on pt/03/.01.

--- autobackup.bat
+++ autobackup.bat
@@ -1,11 +1,13 @@
+@echo off
 cd "C:\Program Files (x86)\Steam\userdata"
+cmd /c (
+  git add .
+  set time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+  set time=%time: =0%
 
-git add .
-set time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
-set time=%time: =0%
+  set date=%DATE:~0,2%/%DATE:~3,2%/%DATE:~6,4%
+  set date=%date: =0%
 
-set date=%DATE:~0,2%/%DATE:~3,2%/%DATE:~6,4%
-set date=%date: =0%
-
-git commit -am "Regular auto-commit at %time% on %date%"
-git push origin master
+  git commit -am "Regular auto-commit at %time% on %date%"
+  git push origin master
+)
