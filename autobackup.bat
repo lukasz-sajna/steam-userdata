0f45642cc1c3f38ea8dd7bea986a9688c10430cb
Lukasz Sajna <lukasz.sajna@gmail.com>
2025-02-13 03:05:28 +0100

    Regular auto-commit at 03:05:28 on  1/zw/.02.

--- autobackup.bat
+++ autobackup.bat
@@ -2,11 +2,13 @@
 cd "C:\Program Files (x86)\Steam\userdata"
 cmd /c (
   git add .
+
+  :: Pobranie poprawnego formatu godziny
   set time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
   set time=%time: =0%
 
-  set date=%DATE:~0,2%/%DATE:~3,2%/%DATE:~6,4%
-  set date=%date: =0%
+  :: Pobranie poprawnego formatu daty dla polskiego systemu
+  set date=%DATE:~4,2%/%DATE:~1,2%/%DATE:~7,4%
 
   git commit -am "Regular auto-commit at %time% on %date%"
   git push origin master
